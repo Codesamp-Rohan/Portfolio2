0246d673ab4f8667277943c74f4712cb772bd291
DocYard <rohanchaudhary.bkbiet2022@gmail.com>
2023-04-18 14:49:58 +0530

    1

---
+++ script.js
@@ -0,0 +1,15 @@
+//Navbar stick on Scroll
+window.onscroll = function() {myFunction()};
+
+
+var navbar = document.getElementById("navbar");
+
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
